f79599f171119959b7f8b51a3a0666faa057ef12
Влад <uladzislau.volkau@gmail.com>
2019-07-11 23:00:42 +0300

    Commit

--- src/ActionsDemo.java
+++ src/ActionsDemo.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -14,6 +15,13 @@ public class ActionsDemo {
         driver.get("https://www.amazon.com/");
         Actions a = new Actions(driver);
         a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox")))
+                .click()
+                .keyDown(Keys.SHIFT)
+                .sendKeys("Google Home")
+                .doubleClick().build().perform();
+        a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).contextClick().build().perform();
+
 
     }
 
